b778f7d4deae8c3fcd8787775a4eae7cab479114
tablecell <yak_czh@163.com>
2021-10-17 10:03:21 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,106 +1,91 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	"github.com/azer/crud"
+
 	_ "github.com/mattn/go-sqlite3"
-	"github.com/xujiajun/gorouter"
+
 	"html/template"
 	"log"
 	"net/http"
-	"os"
-	"strconv"
 	"time"
 )
+
 type (
 	Jobs struct {
-		Id          int       `sql:"auto_increment primary-key"`
-		Title       string    `db:"title"`
-		Salarytop   int       `db:"salarytop"`
-		Salarybase  int       `db:"salarybase"`
+		Id         int    `sql:"auto_increment primary-key"`
+		Title      string `db:"title"`
+		Salarytop  int    `db:"salarytop"`
+		Salarybase int    `db:"salarybase"`
+		Mail       string `db:"mail"`
+		Gender     int    `db:"gender"`
+		Is996      int    `db:"is996"`
+		Level      int    `db:"level"`
 		Description string    `db:"description"`
+		Remote      string    `db:"remote"`
 		Published   time.Time `db:"published"`
 	}
 	TplData struct {
-		Jobs []*Jobs
+		Jobs []Jobs
 	}
 )
 
 var (
-	DB               *crud.DB
+ 
 	htmlTplEngine    *template.Template
 	htmlTplEngineErr error
 )
-func ServeFiles(w http.ResponseWriter, r *http.Request) {
-	wd, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err)
-	}
 
-	dir := wd + "/assets"
-	fmt.Println(dir)
-	http.StripPrefix("/assets/", http.FileServer(http.Dir(dir))).ServeHTTP(w, r)
-}
-
+ 
 func main() {
-
-	var err error
-	DB, err := crud.Connect("sqlite3", "jobs.db")
-	//	err = DB.Ping()
-	fmt.Println(err)
+ 
+	fs := http.FileServer(http.Dir("assets/")) //real dir
+	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
 
 	htmlTplEngine = template.New("htmlTplEngine")
 
-	// 模板根目录下的模板文件 一些公共文件
+	db, err := sql.Open("sqlite3", "jobs.db")
+	fmt.Println(err)
 	_, htmlTplEngineErr = htmlTplEngine.ParseGlob("views/*.html")
 	if nil != htmlTplEngineErr {
 		log.Panic(htmlTplEngineErr.Error())
 	}
 
-	mux := gorouter.New()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
-	mux2 := mux.Group("/assets")
-	mux2.GET("/{filename:[0-9a-zA-Z_.-]+}", func(w http.ResponseWriter, r *http.Request) {
-		ServeFiles(w, r)
-	})
+		//mux.GET("/", func(w http.ResponseWriter, r *http.Request) {
+		jobs := []Jobs{}
 
-	mux.GET("/", func(w http.ResponseWriter, r *http.Request) {
-		jobs := []*Jobs{}
+		result, err := db.Query("SELECT  title,description, salarybase,salarytop,published FROM jobs WHERE id > ?", 0)
 
-		err := DB.Read(&jobs, "SELECT  * FROM jobs WHERE id > ?", 0)
-		fmt.Println(err)
-		data := TplData{Jobs: jobs}
-
-		_ = htmlTplEngine.ExecuteTemplate(w, "home.html", data)
- 
-	})
-
-	mux.POST("/new", func(w http.ResponseWriter, r *http.Request) {
-		// title
-		// fmt. r.FormValue("title")
-		base, err := strconv.Atoi(r.FormValue("salarybase"))
-		top, err := strconv.Atoi(r.FormValue("salarytop"))
-		fmt.Println(err)
-		var book = Jobs{
-			Title:       r.FormValue("title"),
-			Description: r.FormValue("jd"),
-			Published:   time.Now(),
-			Salarybase:  base,
-			Salarytop:   top,
+		for result.Next() {
+			var j Jobs
+			err := result.Scan( &j.Title,&j.Description, &j.Salarybase, &j.Salarytop,    &j.Published) // check err
+			fmt.Println(err)
+			jobs = append(jobs, j)
 		}
 
-		err = DB.Create(book)
 		fmt.Println(err)
- 		http.Redirect(w, r, "/", 302)
-//		tmpl := template.Must(template.ParseFiles("./test.html"))
-//		tmpl.Execute(w, struct{ Success bool }{true})
+		data := TplData{Jobs: jobs}
+		_ = htmlTplEngine.ExecuteTemplate(w, "home.html", data)
+
 	})
 
-	mux.GET("/new", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/new", func(w http.ResponseWriter, r *http.Request) {
+	
+	
+		if "POST" == r.Method {
+                fmt.Println(r.FormValue("is996"))
+			result, err := db.Exec(`INSERT INTO jobs(title, mail,description, salarybase,salarytop,is996,published) VALUES (?, ?, ?,?,?,?,?)`, r.FormValue("title"), r.FormValue("email"),r.FormValue("jd"), r.FormValue("salarybase"), r.FormValue("salarytop"), r.FormValue("is996"),time.Now())
+
+			fmt.Println(result, err)
+			http.Redirect(w, r, "/", 302)
+		}
+
 		_ = htmlTplEngine.ExecuteTemplate(w, "new.html", nil)
 	})
 
-
-    fmt.Println("ListenAndServe:  http://localhost:8181")
-	log.Fatal(http.ListenAndServe(":8181", mux))
+	fmt.Println("ListenAndServe:  http://localhost:8181")
+	log.Fatal(http.ListenAndServe(":8181", nil))
 }
